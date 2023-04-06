a53db56170eeb503b803d17e5522e2987c7973de
Chakis0 <seritokuk@mail.ru>
2023-04-06 08:54:09 +0300

    Update 06.04

---
+++ data.py
@@ -0,0 +1,53 @@
+# import webbrowser
+# import telebot
+# from telebot import types
+
+# bot = telebot.TeleBot('5926706182:AAGk3M0SxGNLkKxvz-TppnO78bjZcws_B6M')
+
+
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     markup = types.ReplyKeyboardMarkup()
+#     btn1 = types.KeyboardButton('Мой VK')
+#     markup.row(btn1)
+#     btn2 = types.KeyboardButton('Удалить фото')
+#     btn3 = types.KeyboardButton('Изменить текст')
+#     markup.row(btn2, btn3)
+#     file = open('E:/TG work/ChakisTgBot/img/ava.jpg', 'rb')
+#     bot.send_photo(message.chat.id, file, reply_markup=markup)
+#    #  bot.send_audio(message.chat.id, file, reply_markup=markup)
+#    #  bot.send_video(message.chat.id, file, reply_markup=markup)
+#    #  bot.send_message(message.chat.id, 'Hi', reply_markup=markup)
+#     bot.register_next_step_handler(message, on_click)
+
+
+# def on_click(message):
+#     if message.text == 'Мой VK':
+#         bot.send_message(message.chat.id, 'Websiteis open')
+#     elif message.text == 'Удалить фото':
+#         bot.send_message(message.chat.id, 'delete photo')
+
+
+# @bot.message_handler(content_types=['photo'])
+# def get_photo(message):
+#     markup = types.InlineKeyboardMarkup()
+#     btn1 = types.InlineKeyboardButton('Мой VK', url='https://vk.com/chakis0')
+#     markup.row(btn1)
+#     btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
+#     btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
+#     markup.row(btn2, btn3)
+
+#     bot.reply_to(message, 'Вахх какой сочный!', reply_markup=markup)
+
+
+# @bot.callback_query_handler(func=lambda callback: True)
+# def callback_message(callback):
+#     if callback.data == 'delete':
+#         bot.delete_message(callback.message.chat.id,
+#                            callback.message.message_id - 1)
+#     elif callback.data == 'edit':
+#         bot.edit_message_text(
+#             'Edit text', callback.message.chat.id, callback.message.message_id)
+
+
+# bot.polling(none_stop=True)
--- main.py
+++ main.py
@@ -1,51 +1,69 @@
-import webbrowser
 import telebot
-from telebot import types
+import sqlite3
 
 bot = telebot.TeleBot('5926706182:AAGk3M0SxGNLkKxvz-TppnO78bjZcws_B6M')
+name = None
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('Мой VK')
-    markup.row(btn1)
-    btn2 = types.KeyboardButton('Удалить фото')
-    btn3 = types.KeyboardButton('Изменить текст')
-    markup.row(btn2, btn3)
-    file = open('E:/TG work/ChakisTgBot/img/ava.jpg', 'rb')
-    bot.send_photo(message.chat.id, file, reply_markup=markup)
-   #  bot.send_message(message.chat.id, 'Hi', reply_markup=markup)
-    bot.register_next_step_handler(message, on_click)
+    conn = sqlite3.connect('itproger.sql')
+    cur = conn.cursor()
+
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS users(id int auto_increment primary key, name varchar(50), pass varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close
+
+    bot.send_message(
+        message.chat.id, 'Привет, сейчас тебя зарегестрируем! Введите ваше имя')
+    bot.register_next_step_handler(message, user_name)
 
 
-def on_click(message):
-    if message.text == 'Мой VK':
-        bot.send_message(message.chat.id, 'Websiteis open')
-    elif message.text == 'Удалить фото':
-        bot.send_message(message.chat.id, 'delete photo')
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(
+        message.chat.id, 'Введите пароль')
+    bot.register_next_step_handler(message, user_pass)
 
 
-@bot.message_handler(content_types=['photo'])
-def get_photo(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton('Мой VK', url='https://vk.com/chakis0')
-    markup.row(btn1)
-    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
-    btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
-    markup.row(btn2, btn3)
+def user_pass(message):
+    password = message.text.strip()
 
-    bot.reply_to(message, 'Вахх какой сочный!', reply_markup=markup)
+    conn = sqlite3.connect('itproger.sql')
+    cur = conn.cursor()
+
+    cur.execute('INSERT INTO users(name, pass) VALUES ("%s", "%s")' %
+                (name, password))
+    conn.commit()
+    cur.close()
+    conn.close
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(
+        'Список пользователей', callback_data='users'))
+    bot.send_message(
+        message.chat.id, 'Пользователь зарегестрирован', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda callback: True)
-def callback_message(callback):
-    if callback.data == 'delete':
-        bot.delete_message(callback.message.chat.id,
-                           callback.message.message_id - 1)
-    elif callback.data == 'edit':
-        bot.edit_message_text(
-            'Edit text', callback.message.chat.id, callback.message.message_id)
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    conn = sqlite3.connect('itproger.sql')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+
+    info = ''
+    for el in users:
+        info += f'Имя: {el[1]}, пароль: {el[2]}\n'
+
+    cur.close()
+    conn.close
+
+    bot.send_message(call.message.chat.id, info)
 
 
 bot.polling(none_stop=True)
